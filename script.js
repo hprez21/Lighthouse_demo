d884af08de12b5892cd42cd62df5d229b033b403
Héctor Pérez <hectorperez_11c@cenidet.edu.mx>
2022-10-17 11:37:38 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+mineBitcoin(1500);
+
+function mineBitcoin(duration) {
+    const start = new Date().getTime();
+    while (new Date().getTime() < start + duration) {
+    
+    } // TODO: Make $$$
+}
